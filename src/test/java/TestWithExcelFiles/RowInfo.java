c90b1d643020fee50e6cbb56d97e46212ff342aa
Oleg <SolOleg86@gmail.com>
2019-06-03 12:57:43 +0200

    new one

--- src/test/java/TestWithExcelFiles/RowInfo.java
+++ src/test/java/TestWithExcelFiles/RowInfo.java
@@ -1,13 +1,8 @@
 package TestWithExcelFiles;
 
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class RowInfo {
