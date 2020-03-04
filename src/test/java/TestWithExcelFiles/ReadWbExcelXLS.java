3ffbd65a466e78bc635c2a1035e3e7dba5befc46
olegsolodovnikov <SolOleg86@gmail.com>
2020-03-04 13:24:50 +0100

    added BranchComparison for tests

--- src/test/java/TestWithExcelFiles/ReadWbExcelXLS.java
+++ src/test/java/TestWithExcelFiles/ReadWbExcelXLS.java
@@ -17,41 +17,49 @@ public class ReadWbExcelXLS {
         ArrayList<String> allValues = new ArrayList<>();
         String fileNameTXT = "/Users/olegsolodovnikov/Desktop/fcrs_cic/templates/File.txt";
         allValues.addAll(fileToList(fileNameTXT));
-        HashMap <String,String> replaceValues = new HashMap<>();
+        HashMap<String, String> replaceValues = new HashMap<>();
         for (int i = 0; i < allValues.size(); i++) {
-            replaceValues.put(allValues.get(i).replaceAll("\t.*",""),allValues.get(i).replaceAll(".*\t",""));
+            replaceValues.put(allValues.get(i).replaceAll("\t.*", ""), allValues.get(i).replaceAll(".*\t", ""));
         }
-        List <String> replaceableValues = new ArrayList<>();
+        List<String> replaceableValues = new ArrayList<>();
         for (int i = 0; i < allValues.size(); i++) {
-            replaceableValues.add(allValues.get(i).replaceAll("\t.*",""));
+            replaceableValues.add(allValues.get(i).replaceAll("\t.*", ""));
         }
-        String fileNamepathXLS = "/Users/olegsolodovnikov/Desktop/fcrs_cic/templates/CRS_CIC_TEST1.xlsx";
-        FileInputStream finXLS = new FileInputStream(fileNamepathXLS);
+        String fileNamePathXLS = "/Users/olegsolodovnikov/Desktop/fcrs_cic/templates/CRS_CIC_TEST1.xlsx";
+        FileInputStream finXLS = new FileInputStream(fileNamePathXLS);
         XSSFWorkbook wbXLS = new XSSFWorkbook(finXLS);
         for (int i = 0; i < wbXLS.getNumberOfSheets(); i++) {
-            if(wbXLS.getSheetName(i).equals("Account Information")) {
-                XSSFSheet sheetXLS = wbXLS.getSheetAt(i);
-                for (int j = sheetXLS.getLastRowNum(); j < sheetXLS.getLastRowNum(); j++) {
+            XSSFSheet sheetXLS = wbXLS.getSheetAt(i);
+            System.out.println(sheetXLS.getSheetName());
+            for (int j = sheetXLS.getFirstRowNum(); j <= sheetXLS.getLastRowNum(); j++) {
+                if (sheetXLS.getRow(j) != null) {
                     XSSFRow rowXLS = sheetXLS.getRow(j);
+                    System.out.println(sheetXLS.getSheetName() + " : "+ rowXLS.getLastCellNum());
                     for (int k = rowXLS.getFirstCellNum(); k < rowXLS.getLastCellNum(); k++) {
-                        XSSFCell cellXLS = rowXLS.getCell(k);
-                        if (cellXLS.CELL_TYPE_STRING == 1) {
-                            if (valueExists(cellXLS.getStringCellValue(), replaceableValues)) {
-                                cellXLS.setCellValue(replaceValues.get(cellXLS.getStringCellValue()));
+                        if (rowXLS.getCell(k) != null) {
+                            XSSFCell cellXLS = rowXLS.getCell(k);
+                            if (cellXLS.getCellType() == cellXLS.CELL_TYPE_STRING) {
+                                if (valueExists(cellXLS.getStringCellValue(), replaceableValues)) {
+                                    cellXLS.setCellValue(replaceValues.get(cellXLS.getStringCellValue()));
+                                }
                             }
+                        }
+                        else{
+                            continue;
                         }
                     }
                 }
-            }
-            else{
-                continue;
+                else{
+                    continue;
+                }
             }
         }
         FileOutputStream xlsOutPutStream = new FileOutputStream("/Users/olegsolodovnikov/Desktop/fcrs_cic/templates/CRS_CIC_TEST1UPD.xlsx");
         wbXLS.write(xlsOutPutStream);
         xlsOutPutStream.close();
     }
-    public static List <String> fileToList(String fileNamePath) throws IOException {
+
+    public static List<String> fileToList(String fileNamePath) throws IOException {
         FileReader fileReader = new FileReader(fileNamePath);
         BufferedReader reader = new BufferedReader(fileReader);
         List<String> list = new ArrayList<>();
@@ -61,10 +69,11 @@ public class ReadWbExcelXLS {
         }
         return list;
     }
-    public static boolean valueExists (String checkedValueString,List<String>checkedValuesArr){
+
+    public static boolean valueExists(String checkedValueString, List<String> checkedValuesArr) {
         boolean result = false;
         for (int i = 0; i < checkedValuesArr.size(); i++) {
-            if(checkedValueString.equals(checkedValuesArr.get(i))){
+            if (checkedValueString.equals(checkedValuesArr.get(i))) {
                 result = true;
                 break;
             }
